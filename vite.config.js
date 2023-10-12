6244adf8a3315de16eb08a2ee16993b414d1d16b
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-10-12 16:20:26 +0200

    test 3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import scss from "@vitejs/plugin-scss";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), scss()],
   base: "/todo-app",
 });
