d5879600e87a356df630b2f3bf25fafc0dcaa2d3
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-10-18 10:00:15 +0200

    test 3

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./views/LandingPage/LandingPage";
-import TodoListPage from "./views/TodoListPage/TodoListPage";
+// import TodoListPage from "./views/TodoListPage/TodoListPage";
 
 const RouterComponent = () => {
   return (
     <Routes>
       <Route index element={<LandingPage />} />
-      <Route path="todolist" element={<TodoListPage />} />
+      {/* <Route path="todolist" element={<TodoListPage />} /> */}
     </Routes>
   );
 };
